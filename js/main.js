263939f60beda7326e122024a2cb83768c776d72
wojtyyla <96237480+wojtyyla@users.noreply.github.com>
2022-02-04 09:46:59 +0100

    responsive nav 3

--- js/main.js
+++ js/main.js
@@ -2,14 +2,19 @@
 
 const btnhamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 btnhamburger.addEventListener('click', function(){
     console.log('click hamburger');
 
-    if(header.classList.contains('open')){
+    if(header.classList.contains('open')){ // Close hamburger menu
       header.classList.remove('open');
+      overlay.classList.remove('fade-in');
+      overlay.classList.add('fade-out');
     }
-    else {
+    else { // Open hamburger menu
         header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
     }
 });
