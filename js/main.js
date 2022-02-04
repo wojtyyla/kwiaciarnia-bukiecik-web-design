b8172f5130d6c654e50722d98c7ab791742214b9
wojtyyla <96237480+wojtyyla@users.noreply.github.com>
2022-02-04 12:56:32 +0100

    responsive nav 4

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // Navbar
 
 const btnhamburger = document.querySelector('#btnHamburger');
+const html = document.querySelector('html');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 
@@ -8,11 +10,15 @@ btnhamburger.addEventListener('click', function(){
     console.log('click hamburger');
 
     if(header.classList.contains('open')){ // Close hamburger menu
+      html.classList.remove('noscroll');
+      body.classList.remove('noscroll');
       header.classList.remove('open');
       overlay.classList.remove('fade-in');
       overlay.classList.add('fade-out');
     }
     else { // Open hamburger menu
+        html.classList.add('noscroll');
+        body.classList.add('noscroll');
         header.classList.add('open');
         overlay.classList.remove('fade-out');
         overlay.classList.add('fade-in');
