cf476f22a777e7409aa49226d521610d64e84d09
wojtyyla <96237480+wojtyyla@users.noreply.github.com>
2022-02-09 19:32:47 +0100

    Realizacje page - gallery add

--- js/main.js
+++ js/main.js
@@ -28,3 +28,50 @@ btnhamburger.addEventListener('click', function(){
 // Footer date
 
 document.getElementById("js-year").innerHTML = new Date().getFullYear();
+
+// REALIZACJE PAGE Galeria
+
+const filterItem = document.querySelector(".realizacje-page-menu");
+const filterImg = document.querySelectorAll(".gallery-image");
+
+window.onload = ()=>{
+    filterItem.onclick = (selectedItem)=>{
+      if(selectedItem.target.classList.contains("item")){
+        filterItem.querySelector(".active").classList.remove("active");
+        selectedItem.target.classList.add("active");
+        let filterName = selectedItem.target.getAttribute("data-name");
+        filterImg.forEach((image)=>{
+          let filterImages = image.getAttribute("data-name");
+          if ((filterImages == filterName) || filterName == "all"){
+            image.classList.remove("hide");
+            image.classList.add("show");
+          }else{
+            image.classList.add("hide");
+            image.classList.remove("show");
+          }
+        });
+      }
+    }
+    for (let index = 0; index < filterImg.length; index++) {
+      filterImg[index].setAttribute("onclick", "preview(this)");
+
+    }
+}
+
+const previewBox = document.querySelector(".preview-box"),
+previewImg = previewBox.querySelector("img"),
+closeIcon = previewBox.querySelector("i"),
+shadow = document.querySelector(".shadow");
+
+function preview(element){
+  let selectedPrevImg = element.querySelector("img").src;
+  previewImg.src = selectedPrevImg;
+  console.log(selectedPrevImg);
+  previewBox.classList.add("show");
+  shadow.classList.add("show");
+  closeIcon.onclick = ()=>{
+    previewBox.classList.remove("show");
+    shadow.classList.remove("show");
+  }
+}
+
